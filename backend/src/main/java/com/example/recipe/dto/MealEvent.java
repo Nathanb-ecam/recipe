ce5f549167fed6b923ea366b0f6f6c114286a8f6
Nathanb-ecam <nath.buchin@gmail.com>
2025-05-15 11:36:43 +0200

    added eventName to MealEvent

--- backend/src/main/java/com/example/recipe/dto/MealEvent.java
+++ backend/src/main/java/com/example/recipe/dto/MealEvent.java
@@ -1,10 +1,12 @@
 package com.example.recipe.dto;
 
+import com.example.recipe.model.MealType;
 import lombok.Data;
 
 @Data
 public class MealEvent {
-    private String hourMinString; //"12h30"
+    /*private String hourMinString; //"12h30"*/
+    private MealType mealType; // LUNCH, BREAKFAST OR DINNER
     private String eventName; // if the user doesn't need to attach a specific recipe, he can simply set a name
     private String recipeId;
 }
